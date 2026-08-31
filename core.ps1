0ceedc09a0a28289b674cc2801abadb081366590
Cyberman <lomcarlo@users.noreply.github.com>
2026-08-31 18:31:42 +0200

    Add Subtitle Edit to install and uninstall lists

--- core.ps1
+++ core.ps1
@@ -435,6 +435,7 @@ $MenuItems = @{
         @{ Name = "OBS Studio"; ScriptBlock = { Install-Sw "OBS Studio (Registrazione dello schermo)" "OBSProject.OBSStudio" } }
         @{ Name = "VLC Player"; ScriptBlock = { Install-Sw "VLC Video Player" "VLC.VLC" } }
         @{ Name = "Shutter Encoder"; ScriptBlock = { Install-Sw "Shutter Encoder" "PaulPacifico.ShutterEncoder" } }
+		@{ Name = "Subtitle Edit"; ScriptBlock = { Install-Sw "Subtitle Edit" "Nikse.SubtitleEdit" } }
     )
     
     "SOFTWARE 3D" = @(
@@ -545,6 +546,7 @@ $MenuItems = @{
         @{ Name = "OBS Studio"; ScriptBlock = { Uninstall-Sw "OBS Studio (Registrazione dello schermo)" "OBSProject.OBSStudio" } }
         @{ Name = "VLC Player"; ScriptBlock = { Uninstall-Sw "VLC Video Player" "VLC.VLC" } }
         @{ Name = "Shutter Encoder"; ScriptBlock = { Uninstall-Sw "Shutter Encoder" "PaulPacifico.ShutterEncoder" } }
+		@{ Name = "Subtitle Edit"; ScriptBlock = { Uninstall-Sw "Subtitle Edit" "Nikse.SubtitleEdit" } }
         @{ Name = "Visual Studio Code"; ScriptBlock = { Uninstall-Sw "Microsoft Visual Studio Code" "Microsoft.VisualStudioCode" } }
         @{ Name = "FileZilla"; ScriptBlock = { Uninstall-Sw "FileZilla" "FileZilla.FileZilla.Client" } }
         @{ Name = "PrusaSlicer"; ScriptBlock = { Uninstall-Sw "PrusaSlicer" "Prusa3D.PrusaSlicer" } }
